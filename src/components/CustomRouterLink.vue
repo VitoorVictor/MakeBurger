ab482e1e19ae41546f003dc3a0ed9d9c2ede43a6
Vitor Gabriel Blasques Victor <vitorvictorblasq@gmail.com>
2024-08-09 01:37:36 -0300

    Commit

---
+++ src/components/CustomRouterLink.vue
@@ -0,0 +1,23 @@
+<template>
+    <router-link :to="to" :class="{ 'footer': applyCss }">Pedidos</router-link>
+  </template>
+  <script>
+  export default {
+    props: {
+      to: {
+        type: String,
+        required: true
+      },
+      applyCss: {
+        type: Boolean,
+        default: false
+      }
+    }
+  };
+  </script>
+  <style scoped>
+    .footer {
+        position: absolute;
+    }
+  </style>
+  
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Componente de Dashboard</h1>
+</template>
+<script>
+export default {
+    name: "Dashboard"
+}
+</script>
+<style>
+
+</style>
